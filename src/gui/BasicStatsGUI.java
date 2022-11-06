00b15aa3fc1b57926d998fe2139682ef60a33817
Piusha Gullapalli <piusha.gullapalli@gmail.com>
2022-11-05 21:19:19 -0400

    Fixing the bug

--- src/gui/BasicStatsGUI.java
+++ src/gui/BasicStatsGUI.java
@@ -52,20 +52,6 @@ public class BasicStatsGUI implements View {
 
         jfMain.getContentPane().add(jpStats, BorderLayout.CENTER);
 
-
-        // Panel with a text field/button to enter numbers and a button to reset the application
-        JButton jbReset = new JButton("Reset");
-        jbReset.addActionListener(new ActionListener() {
-            // The interface ActionListener defines a call-back method actionPerformed,
-            // which is invoked if the user interacts with the GUI component -- in this
-            // case, if the user clicks the button.
-            public void actionPerformed(ActionEvent e) {
-                // Clear the ArrayList and all text fields
-                model.reset();
-
-                update(model);
-            }
-        });
         JTextField jtfNumber = new JTextField(5);
         JButton jbAdd = new JButton("Add number");
         jbAdd.addActionListener(new ActionListener() {
@@ -82,6 +68,21 @@ public class BasicStatsGUI implements View {
                             JOptionPane.ERROR_MESSAGE);
                     throw new RuntimeException(ex);
                 }
+
+                update(model);
+            }
+        });
+
+        // Panel with a text field/button to enter numbers and a button to reset the application
+        JButton jbReset = new JButton("Reset");
+        jbReset.addActionListener(new ActionListener() {
+            // The interface ActionListener defines a call-back method actionPerformed,
+            // which is invoked if the user interacts with the GUI component -- in this
+            // case, if the user clicks the button.
+            public void actionPerformed(ActionEvent e) {
+                // Clear the ArrayList and all text fields
+                model.reset();
+                jtfNumber.setText("");
 
                 update(model);
             }
